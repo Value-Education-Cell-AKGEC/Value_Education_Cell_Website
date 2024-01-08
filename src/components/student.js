684c7b88032ce27731510f5463cb0e1416418b8b
Kartik-Ranaut <ranautk6@gmail.com>
2024-01-08 14:45:01 +0530

    student js created

---
+++ src/components/student.js
@@ -0,0 +1,11 @@
+let student=[
+        {
+            name:"",
+            email:"",
+            domain:"",
+            linkedin:"",
+            instagram:"",
+            image:"",
+            year:""
+        }
+]
