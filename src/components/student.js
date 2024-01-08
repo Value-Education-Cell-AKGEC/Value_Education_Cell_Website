600d9ca79141d1350dcf966c304a16c5a24b067a
Aseem Jain <jainaseem2004@gmail.com>
2024-01-09 02:32:04 +0530

    Added info(Abhishek/Anjali/Harshit/Swati)

--- src/components/student.js
+++ src/components/student.js
@@ -197,6 +197,7 @@ let student2=[
     }
 ]
 
+
 let student3=[
         {
             name:"Ankur Yadav",
@@ -262,4 +263,40 @@ let student3=[
             instagram:"https://instagram.com/ig_kingslayer.sg?igshid=MXdoeGFhNGYxNDlyOQ==",
             image:"/src/components/Student images/Siddharth Gupta.jpg",
         }
+]
+
+
+let student4=[
+    {
+        name:"Abhishek Patel",
+        email:"Abhishek962851@gmail.com",
+        domain:"Photography",
+        linkedin:"https://www.linkedin.com/in/abhishek-patel-30ba69211",
+        instagram:"https://www.instagram.com/invites/contact/?i=1mrmp0urz2xq0&utm_content=ks38wqm",
+        image:"/src/components/Student images/Abhishek Patel.jpg",
+    },
+    {
+        name:"Anjali Tiwari",
+        email:"tanjali16aug@gmail.com",
+        domain:"Oratory",
+        linkedin:"www.linkedin.com/in/anjali-tiwari-clock8",
+        instagram:"https://www.instagram.com/mirror__clock__7/",
+        image:"/src/components/Student images/Anjali Tiwari.jpg",
+    },
+    {
+        name:"Harshit",
+        email:"harshit.vishnoi5588@gmail.com",
+        domain:"Photography",
+        linkedin:" www.linkedin.com/in/harshit-vishnoi-0b4aa7206",
+        instagram:"https://www.instagram.com/camcus_man",
+        image:"/src/components/Student images/Harshit Vishnoi.jpg",
+    },
+    {
+        name:"Swati Saxena",
+        email:"Swatisaxena5104@gmail.com",
+        domain:"Oratory",
+        linkedin:"www.linkedin.com/in/swati-saxena-1b6335213",
+        instagram:"https://www.instagram.com/swatisaxena5104/",
+        image:"/src/components/Student images/Swati Saxena.jpg",
+    }
 ]
