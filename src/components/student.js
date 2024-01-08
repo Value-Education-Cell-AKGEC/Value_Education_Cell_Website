0ad729346a2908ef70fc0f746927e01f8fc85a3d
Aseem Jain <jainaseem2004@gmail.com>
2024-01-08 23:22:29 +0530

    Added info(Aayush/Abhijeet/Ankit/Asmit)

--- src/components/student.js
+++ src/components/student.js
@@ -1,4 +1,41 @@
-// Student 3 is for 3rd year (batch:2025), Student 2 is for 2nd year(batch:2026), Student 4 is for 4th year(batch:2024)
+//Student 2 is for 2nd year(batch:2026) 
+//Student 3 is for 3rd year (batch:2025)
+//Student 4 is for 4th year(batch:2024)
+
+let student2=[
+    {
+        name:"Aayush Gautam",
+        email:"aayush17gautam@gmail.com",
+        domain:"Management",
+        linkedin:"https://www.linkedin.com/in/aayush-gautam-bb674324a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        instagram:"https://instagram.com/__aayush_gautam_?igshid=OGQ5ZDc2ODk2ZA==",
+        image:"/src/components/Student images/Aayush Gautam.jpg",
+    },
+    {
+        name:"Abhijeet Pandey",
+        email:"pandeyabhijeet155@gmail.com",
+        domain:"Oratory",
+        linkedin:"https://www.linkedin.com/in/abhijeet-pandey-7535601b4/",
+        instagram:"https://www.instagram.com/_abhijeeetttt/",
+        image:"/src/components/Student images/Abhijeet Pandey.jpg",
+    },
+    {
+        name:"Ankit Pandey",
+        email:"ankitpandit022003@gmail.com",
+        domain:"Oratory",
+        linkedin:"https://www.linkedin.com/in/ankit-pandey-473197233?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        instagram:"https://instagram.com/ankit8643pandey?igshid=MzMyNGUyNmU2YQ==",
+        image:"/src/components/Student images/Ankit Pandey.jpg",
+    },
+    {
+        name:"Asmit Bhan",
+        email:"askasmit7@gmail.com",
+        domain:"Designing",
+        linkedin:"https://www.linkedin.com/in/asmit-bhan-7769a6213?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        instagram:"https://www.instagram.com/abhi._.1522/",
+        image:"/src/components/Student images/Asmit Bhan.jpg",
+    }
+]
 
 let student3=[
         {
