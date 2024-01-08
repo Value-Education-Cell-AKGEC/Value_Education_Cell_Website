beafec558a09b112174ce753a17cead546083c3c
Aseem Jain <jainaseem2004@gmail.com>
2024-01-08 15:01:22 +0530

    Added info(Aseem)

--- src/components/student.js
+++ src/components/student.js
@@ -1,11 +1,12 @@
-let student=[
+// Student 3 is for 3rd year (batch:2025), Student 2 is for 2nd year(batch:2026), Student 4 is for 4th year(batch:2024)
+
+let student3=[
         {
-            name:"",
-            email:"",
-            domain:"",
-            linkedin:"",
-            instagram:"",
-            image:"",
-            year:""
+            name:"Aseem Jain",
+            email:"jainaseem2004@gmail.com",
+            domain:"Web Development",
+            linkedin:"https://www.linkedin.com/in/aseem-jain-258608252/",
+            instagram:"https://www.instagram.com/aseem2847/",
+            image:"https://drive.google.com/open?id=1U3ykD3zQWYZf_bQVp0j9y9Rd1YclUVOw",
         }
 ]
