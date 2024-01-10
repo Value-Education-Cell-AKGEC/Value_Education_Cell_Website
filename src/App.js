1047ff7b1fd6e35dd4bbcb19389249e117fa01f1
Kartik-Ranaut <ranautk6@gmail.com>
2024-01-10 14:27:41 +0530

    contact us section created

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Activities from "./components/Activities";
 import Team from "./components/Team";
 import Contact from "./components/Contact";
 function App() {
-    let animate=document.getElementById("animate");
+    
     const [themeimage,setthemeimage]=useState(<i class="fa-solid fa-moon"></i>);
     const [theme,setTheme]=useState("lightmode");
     
@@ -14,13 +14,17 @@ function App() {
     const [selectTeam,setTeam]=useState(false);
     const [selectContact,setContact]=useState(false);
 
+ 
+
+
+
     function turnAllFalse(){
       setActivities(false);
       setHome(false);
       setTeam(false);
       setContact(false);
     }
-    function homeHandler(){
+    function homeHandler(btn){
       turnAllFalse();
       setHome(true);
     }
@@ -34,7 +38,7 @@ function App() {
     }
     function contactHandler(){
       turnAllFalse();
-      selectContact(true)
+      setContact(true);
     } 
     function changetheme(){
       if(theme==="darkmode"){
@@ -48,17 +52,16 @@ function App() {
     }
     useEffect(()=>{
       document.body.className=theme;
-    },[theme])
-
+    },[theme]);
   return (
     <div className="app">
       <div className="header">
             <a className="logo" href="index.html"> </a>
             <div className="lnk">
-                <a className="ig home" onClick={homeHandler} href="#home">Home</a>
-                <a className="ig services" onClick={activitiesHandler} href="#service">Activities</a>
-                <a className="ig teams" onClick={teamHandler} href="#tem">Our Team</a>
-                <a className="ig teams" onClick={contactHandler} href="#tem">Contact Us</a>
+                <a className="ig" id="home" onClick={homeHandler} href="#home">Home</a>
+                <a className="ig" id="activity" onClick={activitiesHandler} href="#service">Activities</a>
+                <a className="ig" id="teams" onClick={teamHandler} href="#team">Our Team</a>
+                <a className="ig" id="contact" onClick={contactHandler} href="#contact">Contact Us</a>
             </div>
             <button className="themechangebtn" onClick={changetheme}>{themeimage}</button>
         </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+  
+      <form className='contactform'>
+          <h1>Get in touch</h1>
+          <input type='text' placeholder='Name'></input>
+          <input type='text' placeholder='Email'></input>
+          <textarea type="text" placeholder="content"></textarea>
+          <input type='submit'></input>
+        </form>
+
+      <div className='contactdetails'>
+          <a href="https://www.google.com/maps/place/Ajay+Kumar+Garg+Engineering+College/@28.6711354,77.4928267,15z/data=!4m6!3m5!1s0x390cf30885b1e2a5:0x9983675e24c6638b!8m2!3d28.6756736!4d77.5020041!16zL20vMGJ4aGdf?entry=ttu">
+          <img className="map" src={require(`./images/akgmap.png`)}></img> </a>
+          <div className='link'>
+            <a href="" target="_blank"><i class="fa-brands fa-map" ></i></a>
+            <a href="" target="_blank"><i class="fa-brands fa-instagram" ></i></a>
+            <a href="" target="_blank"><i class="fa-solid fa-envelope" ></i></a>
+          </div>
+      </div>
+    </div>
   )
 }
