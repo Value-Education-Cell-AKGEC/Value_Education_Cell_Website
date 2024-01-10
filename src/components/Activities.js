86006b3b01d8022182d4bcbc2f4c0dbaebc40384
Kartik-Ranaut <ranautk6@gmail.com>
2024-01-10 11:05:39 +0530

    activities section changed

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -2,6 +2,7 @@ import React from 'react'
 import data from './data'
 import Card from './Card'
 export default function Activities() {
+  
   return (
     <div className='activities'>
         <div className='activity'>
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,35 +2,13 @@ import React from 'react'
 
 export default function Card(props) {
 
-  if(props.data%2){
-    return (
-      <div className='card'>
-          <img src={props.image}></img>
-          <div className='sidediv'>
-            <h1>{props.name}</h1>
-            <p>{props.text}</p>
-          </div>
-      </div>
-    )
-  }
-  else{
 
-    
-    return (
-      <div className='card cardright'>
-          <img src={props.image}></img>
+
+  return (
+        <div className="card">
+            <img src={props.image}></img>
             <h1>{props.name}</h1>
             <p>{props.text}</p>
-      </div>
-    )
-  }
-  // return (
-  //       <div className='card'>
-  //           <div className='sidediv'>
-  //             <h1>{props.name}</h1>
-  //             <p>{props.text}</p>
-  //           </div>
-  //           <img src={props.image}></img>
-  //       </div>
-  //     )
+        </div>
+      )
 }
